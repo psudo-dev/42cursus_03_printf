ca30e86f9118551eb37b44d2981c683039c9b99f
Renato Setoue <eu.recebo.spam@gmail.com>
2021-11-30 20:38:37 -0300

    id to spec

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rsetoue <rsetoue@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 23:06:22 by rsetoue           #+#    #+#             */
-/*   Updated: 2021/11/15 23:36:08 by rsetoue          ###   ########.fr       */
+/*   Updated: 2021/11/30 12:49:03 by rsetoue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ typedef struct s_data
 	int				zero_pad;
 }					t_data;
 
-typedef struct s_id
+typedef struct s_spec
 {
 	int				i;
 	char			type;
@@ -45,12 +45,12 @@ typedef struct s_id
 	int				width;
 	int				precision;
 	int				empty;
-}					t_id;
+}					t_spec;
 
 // < printers
 int		ft_printf(const char *string, ...);
-int		ft_printer(va_list ap, t_data **data, t_id **id);
-void	ft_parse_print(t_data **data, t_id **id);
+int		ft_printer(va_list ap, t_data **data, t_spec **spec);
+void	ft_parse_print(t_data **data, t_spec **spec);
 void	ft_print_n_chrs(int n, char c);
 
 // > type char
@@ -58,42 +58,42 @@ void	ft_type_chr(va_list ap, t_data **data);
 void	ft_type_pct(t_data **data);
 void	ft_type_str(va_list ap, t_data **data);
 // > type int
-void	ft_type_dec(va_list ap, t_data **data, t_id **id);
+void	ft_type_dec(va_list ap, t_data **data, t_spec **spec);
 // > type unsigned int
-void	ft_type_uint(va_list ap, t_data **data, t_id **id);
-void	ft_type_hex(va_list ap, t_data **data, t_id **id);
-void	ft_type_ptr(va_list ap, t_data **data, t_id **id);
+void	ft_type_uint(va_list ap, t_data **data, t_spec **spec);
+void	ft_type_hex(va_list ap, t_data **data, t_spec **spec);
+void	ft_type_ptr(va_list ap, t_data **data, t_spec **spec);
 
-// + identifiers
-void	ft_id_check(t_data **data, t_id **id);
-void	ft_type_handler(va_list ap, t_data **data, t_id **id);
-void	ft_id_handler(va_list ap, t_data **data, t_id **id);
+// + specifiers
+void	ft_spec_check(t_data **data, t_spec **spec);
+void	ft_type_handler(va_list ap, t_data **data, t_spec **spec);
+void	ft_spec_handler(va_list ap, t_data **data, t_spec **spec);
 
 // < flags
-void	ft_zero_check(t_id **id);
-void	ft_hash_check(t_id **id);
-void	ft_minus_check(t_id **id);
-void	ft_plus_check(t_id **id);
-void	ft_space_check(t_id **id);
+void	ft_zero_check(t_spec **spec);
+void	ft_hash_check(t_spec **spec);
+void	ft_minus_check(t_spec **spec);
+void	ft_plus_check(t_spec **spec);
+void	ft_space_check(t_spec **spec);
 // < width
-void	ft_width_check(t_data **data, t_id **id);
-void	ft_width_handler(t_data **data, t_id **id);
+void	ft_width_check(t_data **data, t_spec **spec);
+void	ft_width_handler(t_data **data, t_spec **spec);
 // < precision
-void	ft_precision_check(t_data **data, t_id **id);
-void	ft_precision_handler(t_data **data, t_id **id);
+void	ft_precision_check(t_data **data, t_spec **spec);
+void	ft_precision_handler(t_data **data, t_spec **spec);
 
 // ~ structs
 void	ft_free_null(char **str);
 t_data	*ft_new_data(void);
 void	ft_clear_reset_data(t_data **data, int reset);
-t_id	*ft_new_id(void);
-void	ft_clear_reset_id(t_id **id, int reset);
+t_spec	*ft_new_spec(void);
+void	ft_clear_reset_spec(t_spec **spec, int reset);
 
 // ~ utils
 int		ft_str_to_int(char *str);
-int		ft_has_type(t_data **data, t_id **id);
-void	ft_counter(t_data **data, t_id **id);
-void	ft_prefix_handler(t_data **data, t_id **id);
-void	ft_parse_handler(t_data **data, t_id **id);
+int		ft_has_type(t_data **data, t_spec **spec);
+void	ft_counter(t_data **data, t_spec **spec);
+void	ft_prefix_handler(t_data **data, t_spec **spec);
+void	ft_parse_handler(t_data **data, t_spec **spec);
 
 #endif
